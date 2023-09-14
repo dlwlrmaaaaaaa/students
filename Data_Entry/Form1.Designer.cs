bc916915a053b9c4591a7b8a51eefe07e7518f41
toshibaaaaa <kickback0713@gmail.com>
2023-09-14 10:08:25 +0800

    adding some features

--- Data_Entry/Form1.Designer.cs
+++ Data_Entry/Form1.Designer.cs
@@ -35,7 +35,7 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
             this.txtLastName = new System.Windows.Forms.TextBox();
             this.txtAddress = new System.Windows.Forms.TextBox();
             this.txtMiddleName = new System.Windows.Forms.TextBox();
@@ -46,7 +46,7 @@
             this.cmdUpdate = new System.Windows.Forms.Button();
             this.cmdDelete = new System.Windows.Forms.Button();
             this.cmdCancel = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.ListView = new System.Windows.Forms.ListView();
             this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -61,80 +61,80 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(45, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 19);
+            this.label1.Size = new System.Drawing.Size(91, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Student ID:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(42, 63);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 19);
+            this.label2.Size = new System.Drawing.Size(90, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Last Name:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(42, 103);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(90, 19);
+            this.label3.Size = new System.Drawing.Size(90, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "First Name:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(25, 143);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(107, 19);
+            this.label4.Size = new System.Drawing.Size(105, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Middle Name:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(62, 180);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(70, 19);
+            this.label5.Size = new System.Drawing.Size(72, 20);
             this.label5.TabIndex = 4;
             this.label5.Text = "Address:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(64, 222);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(62, 19);
+            this.label6.Size = new System.Drawing.Size(64, 20);
             this.label6.TabIndex = 5;
             this.label6.Text = "Course:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(82, 262);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(44, 19);
+            this.label7.Size = new System.Drawing.Size(47, 20);
             this.label7.TabIndex = 6;
             this.label7.Text = "Year:";
             // 
-            // textBox1
+            // txtID
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(153, 16);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(216, 26);
-            this.textBox1.TabIndex = 7;
+            this.txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(153, 16);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(216, 26);
+            this.txtID.TabIndex = 7;
             // 
             // txtLastName
             // 
@@ -176,6 +176,7 @@
             this.cmdNew.TabIndex = 14;
             this.cmdNew.Text = "NEW";
             this.cmdNew.UseVisualStyleBackColor = true;
+            this.cmdNew.Click += new System.EventHandler(this.cmdNew_Click);
             // 
             // cmdSave
             // 
@@ -213,6 +214,7 @@
             this.cmdDelete.TabIndex = 18;
             this.cmdDelete.Text = "DELETE";
             this.cmdDelete.UseVisualStyleBackColor = true;
+            this.cmdDelete.Click += new System.EventHandler(this.cmdDelete_Click);
             // 
             // cmdCancel
             // 
@@ -223,9 +225,9 @@
             this.cmdCancel.Text = "CANCEL";
             this.cmdCancel.UseVisualStyleBackColor = true;
             // 
-            // listView1
+            // ListView
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.ID,
             this.columnHeader2,
             this.columnHeader3,
@@ -233,16 +235,15 @@
             this.columnHeader5,
             this.columnHeader6,
             this.columnHeader7});
-            this.listView1.FullRowSelect = true;
-            this.listView1.GridLines = true;
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(375, 16);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(413, 422);
-            this.listView1.TabIndex = 20;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.ListView.FullRowSelect = true;
+            this.ListView.GridLines = true;
+            this.ListView.HideSelection = false;
+            this.ListView.Location = new System.Drawing.Point(375, 16);
+            this.ListView.Name = "ListView";
+            this.ListView.Size = new System.Drawing.Size(413, 422);
+            this.ListView.TabIndex = 20;
+            this.ListView.UseCompatibleStateImageBehavior = false;
+            this.ListView.View = System.Windows.Forms.View.Details;
             // 
             // ID
             // 
@@ -306,7 +307,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.cboYear);
             this.Controls.Add(this.cboCourse);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.ListView);
             this.Controls.Add(this.cmdCancel);
             this.Controls.Add(this.cmdDelete);
             this.Controls.Add(this.cmdUpdate);
@@ -317,7 +318,7 @@
             this.Controls.Add(this.txtMiddleName);
             this.Controls.Add(this.txtAddress);
             this.Controls.Add(this.txtLastName);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtID);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -342,7 +343,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtID;
         private System.Windows.Forms.TextBox txtLastName;
         private System.Windows.Forms.TextBox txtAddress;
         private System.Windows.Forms.TextBox txtMiddleName;
@@ -353,7 +354,7 @@
         private System.Windows.Forms.Button cmdUpdate;
         private System.Windows.Forms.Button cmdDelete;
         private System.Windows.Forms.Button cmdCancel;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView ListView;
         private System.Windows.Forms.ColumnHeader ID;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
--- Data_Entry/Form1.cs
+++ Data_Entry/Form1.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Data_Entry
 {
@@ -22,28 +23,18 @@ namespace Data_Entry
          string mycon = "Server=localhost;Database=crud;Uid=root;Pwd=";
          MySqlCommand mycommand;
          MySqlDataReader rdr;
-
         private void Form1_Load(object sender, EventArgs e)
         {
             myconn = new MySqlConnection(mycon);
             myconn.Open();
             loadCourse(cboCourse);
+            loadData();
 
-            cboCourse.Items.Add("BSCS");
             cboYear.Items.Add("1st");
             cboYear.Items.Add("2nd");
             cboYear.Items.Add("3rd");
             cboYear.Items.Add("4th");
         }
-
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-
-
-
         private void cmdSave_Click(object sender, EventArgs e)
         {
             string lname = txtLastName.Text;
@@ -52,40 +43,132 @@ namespace Data_Entry
             string address = txtAddress.Text;
             string course = cboCourse.SelectedItem.ToString();
             string year = cboYear.SelectedItem.ToString();
-
-
-            string sql = "INSERT INTO tbl_students (last_name,first_name,middle_name,address,course,s_year) values " +
-                "('"+lname+"','" + fname + "', '" + mname + "','" + address + "','" + course + "','"+year+"')";
-            myconn = new MySqlConnection(mycon);
-            myconn.Open();
-            mycommand = new MySqlCommand(sql, myconn);
-            mycommand.ExecuteNonQuery();
-            myconn.Close();
-            myconn.Dispose();
+            try
+            {
+                string sql = "INSERT INTO tbl_students (last_name,first_name,middle_name,address,course,s_year) values " +
+                "('" + lname + "','" + fname + "', '" + mname + "','" + address + "','" + course + "','" + year + "')";
+                myconn = new MySqlConnection(mycon);
+                myconn.Open();
+                mycommand = new MySqlCommand(sql, myconn);
+                mycommand.ExecuteNonQuery();
+                MessageBox.Show("Record added!");
+                myconn.Close();
+                myconn.Dispose();
+                resetAll();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occured: " + ex.Message);
+            }
           
         }
-        private void loadCourse(ComboBox combobox)
+        public void loadData()
         {
-            combobox.Items.Clear();
-            string sql = "SELECT * FROM tbl_year";
+            string sql = "SELECT * from tbl_students";
             myconn = new MySqlConnection(mycon);
             myconn.Open();
             mycommand = new MySqlCommand(sql, myconn);
+            ListView.Items.Clear();
             mycommand.ExecuteNonQuery();
             rdr = mycommand.ExecuteReader();
-            while (rdr.Read()){
-                combobox.Items.Add(rdr[1].ToString());
+            while (rdr.Read())
+            {
+                ListViewItem item = new ListViewItem(rdr["id"].ToString());
+                item.SubItems.Add(rdr["first_name"].ToString());
+                item.SubItems.Add(rdr["last_name"].ToString());
+                item.SubItems.Add(rdr["middle_name"].ToString());
+                item.SubItems.Add(rdr["address"].ToString());
+                item.SubItems.Add(rdr["course"].ToString());
+                item.SubItems.Add(rdr["s_year"].ToString());
+                ListView.Items.Add(item);
             }
             myconn.Close();
             myconn.Dispose();
 
-                
-                    
-                    
-                    }
+        }
 
+        private void loadCourse(System.Windows.Forms.ComboBox combobox)
+        {
+            try
+            {
+                combobox.Items.Clear();
+                string sql = "SELECT * FROM tbl_year";
+                myconn = new MySqlConnection(mycon);
+                myconn.Open();
+                mycommand = new MySqlCommand(sql, myconn);
+                mycommand.ExecuteNonQuery();
+                rdr = mycommand.ExecuteReader();
+                while (rdr.Read())
+                {
+                    combobox.Items.Add(rdr[1].ToString());
+                }
+                myconn.Close();
+                myconn.Dispose();
 
+            }catch(Exception ex)
+            {
+                MessageBox.Show("An error occured: " + ex.Message);
+            }
+         }
 
-       
+        public void resetAll()
+        {
+            txtID.Text = "";
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtMiddleName.Text = "";
+            txtAddress.Text = "";
+            cboCourse.Text = "";
+            cboYear.Text = "";
+
+            cmdSave.Enabled = false;
+            cmdNew.Enabled = true;
+            
+        }
+
+        public void BtnDisable()
+        {
+            cmdCancel.Enabled = false;
+            cmdEdit.Enabled = false;
+            cmdNew.Enabled = false;
+            cmdDelete.Enabled = false;
+            cmdUpdate.Enabled = false;
+            txtID.Enabled = false;
+
+        }
+
+        private void cmdNew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = "SELECT max(id) from tbl_students";
+                myconn = new MySqlConnection(mycon);
+                myconn.Open();
+                mycommand = new MySqlCommand(sql, myconn);
+                object maxID = mycommand.ExecuteScalar();
+
+                if(maxID != null && !maxID.Equals(DBNull.Value))
+                {
+                    int nextID = Convert.ToInt32(maxID) + 1;
+                    txtID.Text = nextID.ToString();
+                }
+                else{
+                    txtID.Text = "1";
+                }
+                myconn.Close();
+                myconn.Dispose();
+                BtnDisable();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("An Error Occured: " + ex.Message);
+            }
+        
+        }
+
+        private void cmdDelete_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
